843da6fc7fa333a1eff2875c4e476a0ec033deb7
Adem AKKUŞ <32845253+ademakkus@users.noreply.github.com>
2020-04-12 12:26:10 +0300

    Add files via upload

---
+++ 76_ExtensionMethods/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _76_ExtensionMethods
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string helloString = "Hello, Extension Methods!, are you sure ?";
+			int wordCount = helloString.WordCount();
+			Console.WriteLine(wordCount);
+
+			Console.ReadKey();
+		}
+	}
+}
---
+++ 76_ExtensionMethods/StringExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _76_ExtensionMethods
+{
+	public static class StringExtensions
+	{
+		public static int WordCount(this string str)
+		{
+
+			
+
+			return str.Split(new char[] { ' ', '.', '?', '!' }, StringSplitOptions.RemoveEmptyEntries).Length;
+		
+	}
+	}
+}
