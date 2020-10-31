d4870a851343b57092bdeb8fd227ed83a0d32190
Adem AKKUŞ <32845253+ademakkus@users.noreply.github.com>
2020-10-31 07:57:02 +0200

    Add files via upload

---
+++ 83_Collections/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static  System.Console;
+using static System.ConsoleColor;
+namespace _83_Collections
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			ArrayList elemanlar = new ArrayList()
+			{
+				"Test",
+				true,
+				1,
+				12.34,
+				'C'
+
+			};
+
+
+			ReadKey();
+		}
+	}
+}
