08dc7432ec2859cbd83bc84959ec162c69622422
Adem AKKUŞ <32845253+ademakkus@users.noreply.github.com>
2020-10-31 07:56:16 +0200

    Add files via upload

---
+++ 84_Array0001/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+namespace _84_Array0001
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] array = new[] {1, 12, 3, -4, 23, 5, 123};
+			WriteLine($"{"Index"}{"Value",8}"); ///for columns headings
+
+			for (int counter = 0; counter < array.Length; counter++)
+			{
+				WriteLine($"{counter,5}{array[counter],8}");
+			}
+			ReadKey();
+
+		}
+	}
+}
---
+++ 85_Destructor/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using static  System.Console;
+namespace _85_Destructor
+{
+
+	public class DestructSample
+	{
+		public int X;
+
+		public DestructSample(int i)
+		{
+			X = i;
+		}
+
+		~DestructSample()
+		{
+
+			WriteLine("Destructing "+X);
+		}
+
+		public void Generator(int i)
+		{
+			DestructSample destructSample=new DestructSample(i);
+		}
+	}
+	static class Program
+	{
+		static void Main(string[] args)
+		{
+			var sw=new Stopwatch();
+			
+			DestructSample obj=new  DestructSample(0);
+
+			for (int i = 1; i < 100000; i++)
+			{
+				obj.Generator(i);
+				
+				
+			}
+			WriteLine("Done.");
+			ReadKey();
+		}
+	}
+}
